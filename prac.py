e3b25da72fc183ce30f410711a669324f811d61e
Bhardwajmradul <mradul.bhardwaj2003@gmail.com>
2025-04-22 11:16:21 +0530

    pausing python for now

--- prac.py
+++ prac.py
@@ -11,8 +11,17 @@ class stu:
             raise ValueError("Expected positive value")
         else:
             self.age+=num
-
+    @staticmethod
+    def printName(name):
+        print(name)
+    def __repr__(self):
+        return f"{self.name}: {self.age} "
 l=stu.makeObj(["name",2])
 s1=stu("A",6)
 # s1.addage("-5")
+attri={"Interest":"Data Science", "College":"JSS"}
+for key,value in attri.items():
+    setattr(s1,key,value)
+print(s1.Interest)
 print(l.name,l.age)
+s1.printName("name")
